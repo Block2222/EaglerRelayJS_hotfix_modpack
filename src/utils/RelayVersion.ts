94a4b8a52f31a8f0065c59bb07943eb30b6d5d63
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-11-07 08:18:37 -0600

    rebrand

--- src/utils/RelayVersion.ts
+++ src/utils/RelayVersion.ts
@@ -1,5 +1,5 @@
 export class RelayVersion {
-  public static readonly NAME = 'OpenEaglerRelay';
+  public static readonly NAME = 'EaglerRelayJS';
   public static readonly VERSION = '0.1.0';
   public static readonly BRAND = 'colbster';
   public static readonly PROTOCOL = 1;
